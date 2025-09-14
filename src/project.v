fe543ad8ca7171d23020d18d910c014864420f81
Schreiner Johannes (ATV MC PD CD MET) <Johannes.Schreiner@infineon.com>
2025-09-14 11:46:20 +0200

    initial commit without any assertion in testbench

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_sudoku (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,9 +15,37 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+  reg [4:0] reg_array [8:0][8:0];
+  reg [4:0] current_col;
+  reg [4:0] current_row;
+
+  wire number_valid;
+  wire [3:0] number;
+  assign number_valid = ui_in[4];
+  assign number = ui_in[3:0];
+
+  integer i, j;
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      integer i, j;
+      for (i = 0; i < 9; i = i + 1) begin
+        for (j = 0; j < 9; j = j + 1) begin
+          reg_array[i][j] <= 5'b0000; // Fixed value
+        end
+      end
+      current_col <= 0;
+      current_row <= 0;
+    end else begin
+      if(number_valid) begin
+        reg_array[current_row][current_col] <= number;
+        current_col <= (current_col + 1) % 9;
+        current_row <= current_col == 9 ? current_row + 1 : current_row;
+      end
+    end
+  end
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = ui_in - uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
--- test/test.py
+++ test/test.py
@@ -32,9 +32,31 @@ async def test_project(dut):
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
+    sudoku = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+
+    for i in range(9):
+        for j in range(9):
+            dut.ui_in <= (1 << 4) | (sudoku[i][j] & 0xF)  # Set valid bit + 4-bit number
+            dut.ui_in.value[4] = 1
+            await ClockCycles(dut.clk, 1)
+    
+    dut.ui_in.value[4] = 0
+    await ClockCycles(dut.clk, 1)
+    
+
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
